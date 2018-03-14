6b892488d09ae70ecda24ebb0fad7e61c99904af
Faisal Saeed <faisal.saeed@brainxtech.com>
2018-03-14 16:39:52 +0500

    Modal added, Favicon, Fixes

--- footer.php
+++ footer.php
@@ -1,31 +1,31 @@
-<div id="footer" class="container-fluid" style="border-top: 1px solid #337ab7;">
+<div id="footer" class="container-fluid" >
   <div class="row">
     <div class="col-md-4">
         <a href="https://www.facebook.com/brainxtech/" target="_blank">
-          <img src="img\facebook@2x.png" alt="HTML tutorial" style=" width: 20px; height: 20px;margin:9px 2px 9px 2px;">
+          <img class="footer-img" src="img\facebook@2x.png" alt="HTML tutorial">
         </a>
         <a href="https://www.linkedin.com/company/13258108/" target="_blank">
-          <img src="img\linkedin@2x.png" alt="HTML tutorial" style=" width: 20px; height: 20px;margin:9px 2px 9px 2px;">
+          <img class="footer-img" src="img\linkedin@2x.png" alt="HTML tutorial">
         </a>
         <a href="https://www.instagram.com/brainxtechs/" target="_blank">
-          <img src="img\instagram@2x.png" alt="HTML tutorial" style=" width: 20px; height: 20px;margin:9px 2px 9px 2px;">
+          <img class="footer-img" src="img\instagram@2x.png" alt="HTML tutorial">
         </a>
         <a href="https://twitter.com/BrainXTechs" target="_blank">
-          <img src="img\twitter@2x.png" alt="HTML tutorial" style=" width: 20px; height: 20px;margin:9px 2px 9px 2px;">
+          <img class="footer-img" src="img\twitter@2x.png" alt="HTML tutorial" >
         </a>
-        <p>Made by <b>BrainX Technologies</b> </p>
+        <p>Made by <a href="https://www.brainxtech.com/" target="_blank" id="brainx_link"><b>BrainX Technologies</b></a> </p>
+    </div>
+    
+    <div class="col-md-4 footer-col2" >
+      <p>By using this site you accept the terms of use.</p>
     </div>
 
-    <div class="col-md-4" style=" position: relative;top: 35px;">
-      <p style="position: absolute;">By using this site you accept the terms of use.</p>
-    </div>
-
-    <div class="col-md-4">
-      <a href="#">
-          <img src="img\footer_gray_html_image@2x.png" alt="logo1" style=" height: 27px; margin: 27px 2px 9px 2px; float: right;">
+    <div class="col-md-4 footer-col3">
+      <a href="">
+          <img class="footer-logo" src="img\footer_gray_html_image@2x.png" alt="logo1">
         </a>
-        <a href="#">
-          <img src="img\footer_html_pdf@2x.png" alt="logo1" style=" height: 27px; margin: 27px 2px 9px 2px; float: right;">
+        <a href="">
+          <img class="footer-logo" src="img\footer_html_pdf@2x.png" alt="logo1" >
         </a>
     </div>
   </div>
