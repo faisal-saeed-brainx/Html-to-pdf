fc084ab5e6a7bbf0704fecb0aafb4bfb2048518b
Faisal Saeed <faisal.saeed@brainxtech.com>
2018-03-14 17:45:23 +0500

    Style: step_section->Pills align center

--- steps.php
+++ steps.php
@@ -2,7 +2,7 @@
   <h1 >HOW IT WORKS</h1>
   <div class="col-md-8 container steps-col1">
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-3 pill-col">
         <div  class="pill"><a onclick="document.getElementById('pic').src='img/step1@2x.png'">1</a></div>
       </div>
       <div class="col-md-9">
@@ -10,7 +10,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-3 pill-col">
         <div class="pill"><a onclick="document.getElementById('pic').src='img/step 2@2x.png'">2</a></div>
       </div>
       <div class="col-md-9">
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-3 pill-col">
         <div class="pill"><a onclick="document.getElementById('pic').src='img/step 3@2x.png'">3</a></div>
       </div>
       <div class="col-md-9">
